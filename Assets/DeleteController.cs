1ab69bb6a74ba1a62af5e2f3c8c0b03dc9e62011
natsuya yamashiro <azuki728@me.com>
2019-10-28 20:37:58 +0900

    課題です。Unityちゃんの後ろに横長の箱を設置し、それがUnityちゃんに追随した上でUnityちゃんを通り過ぎたオブジェクトが箱に接触した自転でオブジェクトを削除するようにしました。 よろしくお願い致します。

---
+++ Assets/DeleteController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteController : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんとカメラの距離
+    private float difference;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+        //Unityちゃんとカメラの位置（z座標）の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        //Unityちゃんの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
+    }
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理
+    void OnTriggerEnter(Collider other)
+    {
+
+        //コインに衝突した場合
+        if (other.gameObject.tag == "CoinTag")
+        {
+            //接触したコインのオブジェクトを破棄
+            Destroy(other.gameObject);
+        }
+
+        //車に衝突した場合
+        if (other.gameObject.tag == "CarTag")
+        {
+            //接触した車のオブジェクトを破棄
+            Destroy(other.gameObject);
+        }
+
+        //コーンに衝突した場合
+        if (other.gameObject.tag == "TrafficConeTag")
+        {
+            //接触したコーンのオブジェクトを破棄
+            Destroy(other.gameObject);
+        }
+    }
+}
